1862a6a3842e79c73a08980bd67bb9e73041f788
Issac Cabral <issaccabrall@gmail.com>
2022-10-16 15:27:23 -0300

    chore: add env config

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,34 +1,15 @@
+import env from '../config/env'
 import {DataSource} from 'typeorm'
-
-// import env from '../config/env'
-// const dataSource = new DataSource({
-//     type: 'mysql',
-//     host: env.TYPEORM_HOST,
-//     port: Number(env.TYPEORM_PORT),
-//     username: env.TYPEORM_USERNAME,
-//     password: env.TYPEORM_PASSWORD,
-//     database: env.TYPEORM_DATABASE,
-//     migrations: [__dirname + '/migrations/*{.ts,.js}'],
-//     entities: [ __dirname + "/../app/entities/*{.ts,.js}"],
-// })
 
 const dataSource = new DataSource({
     type: 'mysql',
-    host: 'users-microservice-db.cvnv8efwjlyi.us-east-1.rds.amazonaws.com',
-    port: 3306,
-    username: 'admin',
-    password: '12345678',
-    database: 'app',
+    host: env.TYPEORM_HOST,
+    port: Number(env.TYPEORM_PORT),
+    username: env.TYPEORM_USERNAME,
+    password: env.TYPEORM_PASSWORD,
+    database: env.TYPEORM_DATABASE,
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
     entities: [ __dirname + "/../app/entities/*{.ts,.js}"],
 })
-
-// dataSource.initialize()
-//     .then(() => {
-//         console.log("Data Source has been initialized!")
-//     })
-//     .catch((err) => {
-//         console.error("Error during Data Source initialization", err)
-//     })
 
 export default dataSource

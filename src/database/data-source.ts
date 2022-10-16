9a9d6a7494cea6f2e9f80a53d284a81da44e8f32
Issac Cabral <issaccabrall@gmail.com>
2022-10-16 15:18:41 -0300

    fix: resolve cannot find a module

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,24 @@
-import env from '../config/env'
-
 import {DataSource} from 'typeorm'
+
+// import env from '../config/env'
+// const dataSource = new DataSource({
+//     type: 'mysql',
+//     host: env.TYPEORM_HOST,
+//     port: Number(env.TYPEORM_PORT),
+//     username: env.TYPEORM_USERNAME,
+//     password: env.TYPEORM_PASSWORD,
+//     database: env.TYPEORM_DATABASE,
+//     migrations: [__dirname + '/migrations/*{.ts,.js}'],
+//     entities: [ __dirname + "/../app/entities/*{.ts,.js}"],
+// })
 
 const dataSource = new DataSource({
     type: 'mysql',
-    host: env.TYPEORM_HOST,
-    port: Number(env.TYPEORM_PORT),
-    username: env.TYPEORM_USERNAME,
-    password: env.TYPEORM_PASSWORD,
-    database: env.TYPEORM_DATABASE,
+    host: 'users-microservice-db.cvnv8efwjlyi.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    username: 'admin',
+    password: '12345678',
+    database: 'app',
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
     entities: [ __dirname + "/../app/entities/*{.ts,.js}"],
 })
--- src/routes/main-router.ts
+++ src/routes/main-router.ts
@@ -1,16 +1,20 @@
 import "reflect-metadata"
 import dataSource from "../database/data-source";
 import { Router } from "express";
+import { User } from "../app/entities/User";
 
 const mainRouter = Router()
 
 mainRouter.get("/", async (req, res, next) => {
-  await dataSource.initialize()
+  const connection = await dataSource.initialize()
+
+  const usersRepository = connection.getRepository(User)
+  const users = await usersRepository.find()
 
   console.log('conectado com sucesso ao banco')
 
   return res.status(200).json({
-    message: "Hello from root!",
+    message: users,
   });
 });
 
